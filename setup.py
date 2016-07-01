bb2521ab58e180515784fd03a052a9d7836585b2
hefmio <mio@hef.dk>
2016-07-01 07:52:36 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pyshp',
-      version='1.2.3',
+      version='1.2.4',
       description='Pure Python read/write support for ESRI Shapefile format',
       long_description=open('README.txt').read(),
       author='Joel Lawhead',
